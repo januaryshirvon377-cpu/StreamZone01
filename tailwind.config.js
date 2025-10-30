0288a4c51e00ecd44f9533740e37e7aa9cf33408
januaryshirvon377-cpu <januaryshirvon377@gmail.com>
2025-10-30 08:41:34 -0700

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#000000',
+        accent: '#7c3aed',
+      },
+    },
+  },
+  plugins: [],
+}
